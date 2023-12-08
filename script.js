86579bf16df12d724459abc5bbd619fe67484826
Daniel <127659003+dannycee@users.noreply.github.com>
2023-12-08 20:53:05 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+const smallCups = document.querySelectorAll(".cup-small");
+
+const liters = document.getElementById("liters");
+const percentage = document.getElementById("percentage");
+const remained = document.getElementById("remained");
+
+smallCups.forEach((cup, idx) => {
+  cup.addEventListener("click", () => highlightCups(idx));
+});
+
+// zamalowanie małych kubków.
+function highlightCups(idx) {
+  if (
+    smallCups[idx].classList.contains("full") &&
+    !smallCups[idx].nextElementSibling.classList.contains("full")
+  ) {
+    idx--;
+  }
+
+  smallCups.forEach((cup, idx2) => {
+    // jeśli klikniemy np. w 4 kubek to zamalują się wszystkie pierwsze 4. itd.
+    if (idx2 <= idx) {
+      cup.classList.add("full");
+    } else {
+      cup.classList.remove("full");
+    }
+  });
+
+  updateBigCup();
+}
+
+function updateBigCup() {
+  const fullCups = document.querySelectorAll(".cup-small.full").length;
+
+  const totalCups = smallCups.length;
+  // jesli puste
+  if (fullCups === 0) {
+    percentage.style.visibility = "hidden";
+    percentage.style.height = 0;
+    //  jesli pelne
+  } else {
+    percentage.style.visibility = "visible";
+    percentage.style.height = `${(fullCups / totalCups) * 330}px`;
+    percentage.innerText = `${(fullCups / totalCups) * 100}%`;
+  }
+
+  if (fullCups === totalCups) {
+    remained.style.visibility = "hidden";
+    remained.style.height = 0;
+  } else {
+    remained.style.visibility = "visible";
+    liters.innerText = `${2 - (250 * fullCups) / 1000}`;
+  }
+}
